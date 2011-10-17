b4704c5de239471887ae0158f9a1166beccb57b4
crazysaem <crazysaem@googlemail.com>
2011-10-17 23:38:19 +0200

    skin class introduced

--- src/com/window/erdeedre/_main.java
+++ src/com/window/erdeedre/_main.java
@@ -1,6 +1,6 @@
 package com.window.erdeedre;
 
-import com.window.erdeedre.customwindow.WindowRunnable;
+import com.window.erdeedre.skins.SkinChooser;
 
 /**
  * Main Entry into the Program
@@ -9,36 +9,8 @@ import com.window.erdeedre.customwindow.WindowRunnable;
  *
  */
 public class _main {
+	
 	public static void main(String[] args) {
-		boolean showSplashScreen=true;
-		
-		if(showSplashScreen) {
-			//Create Window, Set Title, Set Shape
-			WindowRunnable splashScreen = new WindowRunnable("Erde-Edre Übersetzer", "src/resources/images/windowlogo_shape.png");
-			//Set Window Background
-			splashScreen.getWindow().SetBackgroundImage("src/resources/images/windowlogo.png");
-			//Show the Window
-			splashScreen.run();    
-			
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			splashScreen.getWindow().setEnabled(false);
-			splashScreen.getWindow().setFocusable(false);
-			splashScreen.getWindow().setVisible(false);
-		}
-		
-		//Create Window, Set Title, Set Shape
-		WindowRunnable mainWindow = new WindowRunnable("Erde-Edre Übersetzer", "src/resources/images/player2_shape.png");
-		//Init Main Window
-		mainWindow.getWindow().initMainWindow();
-		//Set Window Background
-		mainWindow.getWindow().SetBackgroundImage("src/resources/images/player2.png");		
-    	//Show the Window
-		mainWindow.run();    	
+		new SkinChooser(1, true);
     }
 }
